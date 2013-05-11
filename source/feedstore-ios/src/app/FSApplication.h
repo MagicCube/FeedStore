528a18bdf4715e62def1ce4b7c5fd7d35d62111c
Henry Li <henry.li03@sap.com>
2013-05-11 14:12:05 +0800

    Create a new iOS project

---
+++ source/feedstore-ios/src/app/FSApplication.h
@@ -0,0 +1,15 @@
+//
+//  FSAppDelegate.h
+//  FeedStore
+//
+//  Created by Henry Li on 13-5-11.
+//  Copyright (c) 2013年 MagicCube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSApplication : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ source/feedstore-ios/src/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FeedStore
+//
+//  Created by Henry Li on 13-5-11.
+//  Copyright (c) 2013年 MagicCube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FSApplication.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FSApplication class]));
+    }
+}
